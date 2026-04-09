ac3c6fd696947d3fcfc86267170e821ddc11b70b
Nirjaly <nirjaly1212son@outlook.com>
2026-04-09 17:16:20 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+project = 'find-bitlocker-recovery-key'
+author = 'find-bitlocker-recovery-key'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
+
+# Bing search verification
+html_context = {
+    'bing_verification_code': 'E06B837D01BB33B5FE05F701D0E993EE'
+}
+
+# Base URL for sitemap
+html_baseurl = 'https://findbitlockerrecoverykey.readthedocs.io/en/latest/'
